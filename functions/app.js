a48ff22beb171ed49071b1c301251d5ba2a2f130
pleum164 <pleum164@gmail.com>
2019-12-01 00:19:46 +0700

    First commit on reset password, forgot password, mail, user report

--- functions/app.js
+++ functions/app.js
@@ -4,9 +4,6 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}!`)
-})
 
 app.get('/test', (req, res) => {
     res.sendFile(path.join(__dirname,'../test.html'))
