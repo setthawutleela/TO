cab9bc0e0a3a3768c0ef66298aae270aff0b1d9f
abed11 <a.binhossain1@gmail.com>
2019-12-01 18:51:59 +0700

    Abed 1st commit

--- functions/app.js
+++ functions/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const bodyParser = require('body-parser')
 
 const port = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(bodyParser())
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}!`)
@@ -10,4 +14,25 @@ app.listen(port, () => {
 
 app.get('/test', (req, res) => {
     res.sendFile(path.join(__dirname,'../test.html'))
+});
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname,'../signup.html'))
+});
+
+app.get('/personalinfopage', (req, res) => {
+    res.sendFile(path.join(__dirname,'../personalinfopage.html'))
+});
+
+app.get('/editinfo', (req, res) => {
+    res.sendFile(path.join(__dirname,'../editinfo.html'))
+});
+
+
+app.get('/signin', (req, res) => {
+    res.sendFile(path.join(__dirname,'../signin.html'))
+});
+
+app.get('/transit', (req, res) => {
+    res.sendFile(path.join(__dirname,'../transit.html'))
 });
